0c578ae1baf43621f9b9b17827e8547501d1c4ea
muzheb <muzhebm@gamil.com>
2026-01-12 19:36:12 +0530

    security group is added

---
+++ security-group/main.tf
@@ -0,0 +1,18 @@
+data "aws_vpc" "myalreadyvpc" {
+    default = true
+
+  
+}
+resource "aws_security_group" "myownsg" {
+    name = "openallsg"
+    description = "all network sg"
+    vpc_id = data.aws_vpc.myalreadyvpc.id  
+}
+resource "aws_vpc_security_group_ingress_rule" "myingressrule" {
+    security_group_id = aws_security_group.myownsg.id
+    from_port=22
+    to_port=22
+    ip_protocol="tcp"
+    cidr_ipv4="0.0.0.0/0"
+  
+}
